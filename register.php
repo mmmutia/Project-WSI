a3248dd41ab149aeb1ee1fa47690e12def997bfc
Ardella18 <106500964+Ardella18@users.noreply.github.com>
2022-11-30 16:53:40 +0700

    Update register.php

--- register.php
+++ register.php
@@ -11,70 +11,54 @@ if( isset($_POST['register']) ){
 }
 ?>
 
-<!DOCTYPE html>
+<!<!DOCTYPE html>
 <html lang="en">
 
-<head>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link href="css/login.css" rel="stylesheet">
+<!------ Include the above in your HEAD tag ---------->
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <div class="login-wrap">
+	<div class="login-html">
+		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Register</label>
+		<input id="tab-2" type="radio" name="tab" class="for-pwd"><label for="tab-2" class="tab">Forgot Password</label>
+		<div class="login-form">
+			<div class="sign-in-htm">
+				<div class="group">
+					<label for="user" class="label">Fullname</label>
+					<input id="user" type="text" class="input">
+				</div>
+                <div class="group">
+					<label for="user" class="label">Email</label>
+					<input id="user" type="text" class="input">
+				</div>
+				<div class="group">
+					<label for="pass" class="label">Password</label>
+					<input id="pass" type="password" class="input" data-type="password">
+				</div>
+				<div class="group">
+					<input type="submit" class="button" value="Submit">
+				</div>
+				<div class="hr"></div>
+			</div>
+			<div class="for-pwd-htm">
+				<div class="group">
+					<label for="user" class="label">Username or Email</label>
+					<input id="user" type="text" class="input">
+				</div>
+				<div class="group">
+					<input type="submit" class="button" value="Reset Password">
+				</div>
+				<div class="hr"></div>
+			</div>
+		</div>
+	</div>
+</div>
 
-    <title>SB Admin 2 - Register</title>
+</body>
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
-
-<body class="bg-gradient-primary">
-
-    <div class="container">
-
-        <div class="card o-hidden border-0 shadow-lg" style="margin-top: 107px;">
-            <div class="card-body p-0">
-                <!-- Nested Row within Card Body -->
-                <div class="row">
-                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-                    <div class="col-lg-7">
-                        <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
-                            </div>
-                            <form class="user" action="register.php" method="POST">
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="exampleInputUsername"
-                                        placeholder="Username" name="txt_nama">
-                                </div>
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                        placeholder="Email Address" name="txt_email">
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-user" id="exampleInputPassword"
-                                        placeholder="Password" name="txt_pass">
-                                </div>
-                                <button type="submit" name="register" class="btn btn-primary btn-user btn-block">Register</button>
-                            </form>
-                            <hr>
-                            <div class="text-center">
-                                <a class="small" href="login.php">Already have an account? Login!</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
@@ -86,6 +70,31 @@ if( isset($_POST['register']) ){
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
 
+    <script type="text/javascript">
+        $(document).ready(function(){  
+            $('#show_password').on('click', function(){  
+                var passwordField = $('#password');  
+                var passwordFieldType = passwordField.attr('type');
+                if(passwordField.val() != '')
+                {
+                    if(passwordFieldType == 'password')  
+                    {  
+                        passwordField.attr('type', 'text');  
+                        $(this).text('Hide Password');  
+                    }  
+                    else  
+                    {  
+                        passwordField.attr('type', 'password');  
+                        $(this).text('Show Password');  
+                    }
+                }
+                else
+                {
+                    alert("Please Enter Password");
+                }
+            });  
+        }); 
+    </script>
 </body>
 
 </html>
