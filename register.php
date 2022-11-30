c76c1a84b72f1f97285b77a6a59e4f8852ab180f
mmmutia <mutiabudiutami@gmail.com>
2022-11-30 20:09:41 +0700

    Update register.php

--- register.php
+++ register.php
@@ -23,36 +23,30 @@ if( isset($_POST['register']) ){
   <div class="login-wrap">
 	<div class="login-html">
 		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Register</label>
-		<input id="tab-2" type="radio" name="tab" class="for-pwd"><label for="tab-2" class="tab">Forgot Password</label>
+		<input id="tab-2" type="radio" name="tab" class="for-pwd"><label for="tab-2" class="tab"></label>
 		<div class="login-form">
-			<div class="sign-in-htm">
-				<div class="group">
-					<label for="user" class="label">Fullname</label>
-					<input id="user" type="text" class="input">
-				</div>
-                <div class="group">
-					<label for="user" class="label">Email</label>
-					<input id="user" type="text" class="input">
-				</div>
-				<div class="group">
-					<label for="pass" class="label">Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
-				</div>
-				<div class="group">
-					<input type="submit" class="button" value="Submit">
-				</div>
-				<div class="hr"></div>
-			</div>
-			<div class="for-pwd-htm">
-				<div class="group">
-					<label for="user" class="label">Username or Email</label>
-					<input id="user" type="text" class="input">
-				</div>
-				<div class="group">
-					<input type="submit" class="button" value="Reset Password">
-				</div>
-				<div class="hr"></div>
-			</div>
+        <div class="text-center">
+                        <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            </div>
+                            <form class="user" action="register.php" method="POST">
+                            <div class="form-group">
+                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
+                                        placeholder="Email Address" name="txt_email">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-user" id="exampleInputPassword"
+                                        placeholder="Password" name="txt_pass">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="exampleInputUsername"
+                                        placeholder="Username" name="txt_nama">
+                                </div>
+                                <button type="submit" name="register" class="btn btn-primary btn-user btn-block">Register</button>
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="login.php">Already have an account? Login!</a>
+                            </div>
 		</div>
 	</div>
 </div>
