08de9ff1a97ec96ffb88943a323283172a7a5593
Ardella18 <106500964+Ardella18@users.noreply.github.com>
2022-11-30 13:04:06 +0700

    update file index

--- login.php
+++ login.php
@@ -1,170 +1,20 @@
-<?php  
-require('koneksi.php');
-
-session_start();
-
-if (isset($_POST['submit'])) {
-    $email = $_POST['txt_email'];
-    $pass = $_POST['txt_pass'];
-    
-    if (!empty(trim($email)) && !empty(trim($pass))) {
-        $query      = "SELECT * FROM user_detail WHERE user_email = '$email'";
-        $result     = mysqli_query($koneksi, $query);
-        $num        = mysqli_num_rows($result);
-
-        while ($row = mysqli_fetch_array($result)) {
-            $id = $row['id'];
-            $userVal = $row['user_email'];
-            $passVal = $row['user_password'];
-            $userName = $row['user_fullname'];
-            $level = $row['level'];
-
-        }
-
-        if ($num != 0) {
-            if ($userVal==$email && $passVal==$pass) {
-                $_SESSION['id'] = $id;
-                $_SESSION['name'] = $userName;
-                $_SESSION['level'] = $level;
-                header('Location: index.html');
-            }else{
-                $error = 'user atau password salah!!';
-                echo "<script>alert('$error')</script>";
-                header('Location: login.php');
-            }
-        }else{
-            $error = 'user tidak ditemukan!!';
-            echo "<script>alert('$error')</script>";
-            header('Location: login.php');
-        }
-    }else{
-        $error = 'Data tidak boleh kosong!!';
-        echo "<script>alert('$error')</script>";
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Login</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
-    
-    <script>
-        $(document).ready(function(){       
-            $('.form-checkbox').click(function(){
-                if($(this).is(':checked')){
-                    $('.form-password').attr('type','text');
-                }else{
-                    $('.form-password').attr('type','password');
-                }
-            });
-        });
-    </script>
-
-</head>
-
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card border-0 shadow rounded-3 my-5">
-          <div class="card-body p-4 p-sm-5">
-            <h5 class="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Halaman Login</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+   
+    <body>
+        <div class="container">
+          <h1>Login</h1>
             <form>
-              <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
-              </div>
-              <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
-              </div>
-
-              <div class="form-check mb-3">
-                <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
-                <label class="form-check-label" for="rememberPasswordCheck">
-                  Remember password
-                </label>
-              </div>
-              <div class="d-grid">
-                <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Sign
-                  in</button>
-              </div>
-              <hr class="my-4">
-              <div class="d-grid mb-2">
-                <button class="btn btn-google btn-login text-uppercase fw-bold" type="submit">
-                  <i class="fab fa-google me-2"></i> Sign in with Google
-                </button>
-              </div>
-              <div class="d-grid">
-                <button class="btn btn-facebook btn-login text-uppercase fw-bold" type="submit">
-                  <i class="fab fa-facebook-f me-2"></i> Sign in with Facebook
-                </button>
-              </div>
+                <label>Username</label><br>
+                <input type="text"><br>
+                <label>Password</label><br>
+                <input type="password"><br>
+                <button>Log in</button>
             </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</body>
-<!-- Section: Design Block -->
-
-    </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-
-    <script type="text/javascript">
-        $(document).ready(function(){  
-            $('#show_password').on('click', function(){  
-                var passwordField = $('#password');  
-                var passwordFieldType = passwordField.attr('type');
-                if(passwordField.val() != '')
-                {
-                    if(passwordFieldType == 'password')  
-                    {  
-                        passwordField.attr('type', 'text');  
-                        $(this).text('Hide Password');  
-                    }  
-                    else  
-                    {  
-                        passwordField.attr('type', 'password');  
-                        $(this).text('Show Password');  
-                    }
-                }
-                else
-                {
-                    alert("Please Enter Password");
-                }
-            });  
-        }); 
-    </script>
-</body>
-
+        </div>     
+    </body>
 </html>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['username']);
-unset($_SESSION['login']);
-session_destroy();
-header("Location:index.php");
-?>
