dbeec7588e0376545a44ee49639ea583170286c7
ferinaintan <117642482+ferinaintan@users.noreply.github.com>
2022-11-30 17:10:17 +0700

    Create properti_perumahan.sql

---
+++ Database/properti_perumahan.sql
@@ -0,0 +1,290 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2022 at 05:53 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `properti_perumahan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banner`
+--
+
+CREATE TABLE `banner` (
+  `id_banner` int(5) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `tgl_posting` date NOT NULL,
+  `direktori` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fasilitas`
+--
+
+CREATE TABLE `fasilitas` (
+  `id_fasilitas` int(5) NOT NULL,
+  `nama_fasilitas` varchar(200) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foto`
+--
+
+CREATE TABLE `foto` (
+  `id_foto` int(5) NOT NULL,
+  `id_rumah` int(10) NOT NULL,
+  `nama_foto` varchar(100) NOT NULL,
+  `direktori` varchar(100) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level_detail`
+--
+
+CREATE TABLE `level_detail` (
+  `id_level` tinyint(4) NOT NULL,
+  `level` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan_rumah`
+--
+
+CREATE TABLE `pemesanan_rumah` (
+  `id_pemesanan_rumah` int(11) NOT NULL,
+  `nama_pemesan` varchar(100) NOT NULL,
+  `tempat_tgl_lahir` date NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `no_telp_pemesan` varchar(15) NOT NULL,
+  `nama_cluster` varchar(50) NOT NULL,
+  `no_perumahan` varchar(50) NOT NULL,
+  `tgl_pemesanan` date NOT NULL,
+  `nup` varchar(50) NOT NULL,
+  `bukti_pemesanan` varchar(100) NOT NULL,
+  `fotocopy_ktp` varchar(100) NOT NULL,
+  `laporan_pemesanan_rumah` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peta`
+--
+
+CREATE TABLE `peta` (
+  `id_peta` int(5) NOT NULL,
+  `nama_peta` varchar(100) NOT NULL,
+  `direktori` varchar(100) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rumah`
+--
+
+CREATE TABLE `rumah` (
+  `id_rumah` int(10) NOT NULL,
+  `kavling` text NOT NULL,
+  `luas_tanah` float NOT NULL,
+  `luas_bangunan` float NOT NULL,
+  `jumlah_kamar` int(5) NOT NULL,
+  `spesifikasi_ruang` text NOT NULL,
+  `harga` int(20) NOT NULL,
+  `uang_muka` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spesifikasi_teknis`
+--
+
+CREATE TABLE `spesifikasi_teknis` (
+  `id_spesifikasi` int(5) NOT NULL,
+  `id_rumah` int(10) NOT NULL,
+  `pondasi` varchar(300) NOT NULL,
+  `dinding` varchar(300) NOT NULL,
+  `rangka_atap` varchar(300) NOT NULL,
+  `jendela` varchar(300) NOT NULL,
+  `plafond` varchar(300) NOT NULL,
+  `kamar_mandi` varchar(300) NOT NULL,
+  `listrik` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_detail`
+--
+
+CREATE TABLE `user_detail` (
+  `id_user` int(11) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_password` varchar(50) NOT NULL,
+  `user_fullname` varchar(100) NOT NULL,
+  `level` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banner`
+--
+ALTER TABLE `banner`
+  ADD PRIMARY KEY (`id_banner`);
+
+--
+-- Indexes for table `fasilitas`
+--
+ALTER TABLE `fasilitas`
+  ADD PRIMARY KEY (`id_fasilitas`);
+
+--
+-- Indexes for table `foto`
+--
+ALTER TABLE `foto`
+  ADD PRIMARY KEY (`id_foto`),
+  ADD KEY `id_rumah` (`id_rumah`);
+
+--
+-- Indexes for table `level_detail`
+--
+ALTER TABLE `level_detail`
+  ADD PRIMARY KEY (`id_level`);
+
+--
+-- Indexes for table `pemesanan_rumah`
+--
+ALTER TABLE `pemesanan_rumah`
+  ADD PRIMARY KEY (`id_pemesanan_rumah`);
+
+--
+-- Indexes for table `peta`
+--
+ALTER TABLE `peta`
+  ADD PRIMARY KEY (`id_peta`);
+
+--
+-- Indexes for table `rumah`
+--
+ALTER TABLE `rumah`
+  ADD PRIMARY KEY (`id_rumah`);
+
+--
+-- Indexes for table `spesifikasi_teknis`
+--
+ALTER TABLE `spesifikasi_teknis`
+  ADD PRIMARY KEY (`id_spesifikasi`),
+  ADD UNIQUE KEY `id_rumah` (`id_rumah`);
+
+--
+-- Indexes for table `user_detail`
+--
+ALTER TABLE `user_detail`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `level` (`level`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `banner`
+--
+ALTER TABLE `banner`
+  MODIFY `id_banner` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `fasilitas`
+--
+ALTER TABLE `fasilitas`
+  MODIFY `id_fasilitas` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `foto`
+--
+ALTER TABLE `foto`
+  MODIFY `id_foto` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `level_detail`
+--
+ALTER TABLE `level_detail`
+  MODIFY `id_level` tinyint(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pemesanan_rumah`
+--
+ALTER TABLE `pemesanan_rumah`
+  MODIFY `id_pemesanan_rumah` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `peta`
+--
+ALTER TABLE `peta`
+  MODIFY `id_peta` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rumah`
+--
+ALTER TABLE `rumah`
+  MODIFY `id_rumah` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `spesifikasi_teknis`
+--
+ALTER TABLE `spesifikasi_teknis`
+  MODIFY `id_spesifikasi` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `foto`
+--
+ALTER TABLE `foto`
+  ADD CONSTRAINT `foto_ibfk_1` FOREIGN KEY (`id_rumah`) REFERENCES `rumah` (`id_rumah`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `spesifikasi_teknis`
+--
+ALTER TABLE `spesifikasi_teknis`
+  ADD CONSTRAINT `spesifikasi_teknis_ibfk_1` FOREIGN KEY (`id_rumah`) REFERENCES `rumah` (`id_rumah`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
