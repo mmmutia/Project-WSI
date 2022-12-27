b97fb40b1e46e68851c32c35d6b5161d557855aa
ferinaintan <117642482+ferinaintan@users.noreply.github.com>
2022-12-27 11:11:26 +0700

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -8,7 +8,6 @@ $query_mysql = mysqli_query($koneksi,"select * from cluster");
 // $data = mysqli_fetch_array($query_mysql);
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -71,63 +70,27 @@ $query_mysql = mysqli_query($koneksi,"select * from cluster");
           <li><a href="contact.php">Contact Us</a></li>
           <?php
 
-if($userName = $_SESSION['name']){
+          if ($userName = $_SESSION['name']) {
 
-  if($data['level'] == '2'){
-
-    echo "
-    <div class='dropdown' style='margin-right:50px;'><a href='#'> $userName</a>
-      <ul>
-        <li> <a href='profile-user.php'>Profil</a></li>
-        <li> <a href='list-pemesanan.php'>Pemesanan Rumah</a></li>
-        <li> <a href='proggres.php'>Proggres</a></li>
-        <li> <a href='daftar-cluster-tersimpan.php'>Cluster Yang Tersimpan</a></li>
-        <li data-bs-toggle='modal' data-bs-target='#modalLogout'> <a href='javascript:void(0)'>Logout</a></li>
-      </ul>
-    </div>
-    ";
-  }elseif($data['level'] == '3'){
+            echo "
 
   <div class='dropdown' style='margin-right:50px;'><a href='#'> $userName </a>
   <ul>
-  <li> <a href='profile-user.php'>Profil</a></li>
-  <li> <a href='list-pemesanan.php'>Pemesanan Rumah</a></li>
-  <li> <a href='daftar-cluster-tersimpan.php'>Cluster Yang Tersimpan</a></li>
-  <li data-bs-toggle='modal' data-bs-target='#modalLogout'> <a href='javascript:void(0)'>Logout</a></li>
+    <li> <a href='profile-user.php'>Profil</a></li>
+    <li> <a href='list-pemesanan.php'>Pemesanan Rumah</a></li>
+    <li> <a href=''>Cluster Yang Tersimpan</a></li>
+    <li><a href='logout.php'>Logout</a></li>
   </ul>
 </div>
 
-    echo "
-    <div class='dropdown' style='margin-right:50px;'><a href='#'> $userName</a>
-      <ul>
-        <li> <a href='profile-user.php'>Profil</a></li>
-        <li> <a href='list-pemesanan.php'>Pemesanan Rumah</a></li>
-        <li> <a href='proggres_user.php'>Proggres</a></li>
-        <li> <a href='daftar-cluster-tersimpan.php'>Cluster Yang Tersimpan</a></li>
-        <li data-bs-toggle='modal' data-bs-target='#modalLogout'> <a href='javascript:void(0)'>Logout</a></li>
-      </ul>
-    </div>
-    ";
-  }
-  elseif($data['level'] == '1'){
-    echo "
-    <div class='dropdown' style='margin-right:50px;'><a href='#'> $userName</a>
-      <ul>
-        <li> <a href='profile-user.php'>Profil</a></li>
-        <li data-bs-toggle='modal' data-bs-target='#modalLogout'> <a href='javascript:void(0)'>Logout</a></li>
-      </ul>
-    </div>
-    ";
-  }
-
-}else{
-  echo "
+  ";
+          } else {
+            echo "
   <li><a href='login.php'>Login</a></li>
   ";
-}
+          }
 
-?>
-
+          ?>
 
 
 
@@ -168,11 +131,7 @@ if($userName = $_SESSION['name']){
         <div class="col-lg-6">
             <form action="pencarian.php" method="GET">
             <input name="Harga">
-<<<<<<< Updated upstream
             <input name="cluster"> 
-=======
-            <!-- <input name="cluster"> -->
->>>>>>> Stashed changes
            <button>Search</button>
             </form>
           </div>
