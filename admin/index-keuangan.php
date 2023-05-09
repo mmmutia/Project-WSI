2864cdd38bb85fc264bbedf7345a661f25754f39
ardella <ardela01@gmail.com>
2023-05-09 14:43:50 +0700

    Update index-keuangan.php

--- admin/index-keuangan.php
+++ admin/index-keuangan.php
@@ -23,7 +23,7 @@ if (!isset($_SESSION['name'])) {
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Admin Keuangan - Dashboard</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
