95a55e62c3edd1b165f689a9d94eaa6fb5338719
Daffaizath12 <117889603+Daffaizath12@users.noreply.github.com>
2023-05-17 04:52:40 +0700

    API pemesanan dan simpan cluster

---
+++ api/pemesanan_api.php
@@ -0,0 +1,80 @@
+<?php
+// Koneksi ke database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bernady";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Koneksi ke database gagal: " . $conn->connect_error);
+}
+
+// Menangani permintaan POST dari aplikasi Android
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Memeriksa apakah semua data pemesanan ada dalam permintaan
+    if (
+        isset($_POST['nama']) && isset($_POST['alamat']) && isset($_POST['telp']) &&
+        isset($_POST['cluster']) && isset($_POST['pembayaran']) && isset($_POST['tgl']) &&
+        isset($_FILES['gambar'])
+    ) {
+        // Mendapatkan data pemesanan dari permintaan
+        $nama = $_POST['nama'];
+        $alamat = $_POST['alamat'];
+        $telp = $_POST['telp'];
+        $cluster = $_POST['cluster'];
+        $pembayaran = $_POST['pembayaran'];
+        $tgl = $_POST['tgl'];
+        
+        // Mengunggah gambar ke direktori tujuan
+        $uploadDir = 'path/to/destination/directory/';
+        $uploadedFile = $_FILES['gambar']['tmp_name'];
+        $fileName = $_FILES['gambar']['name'];
+        $destination = $uploadDir . $fileName;
+        
+        if (move_uploaded_file($uploadedFile, $destination)) {
+            // Gambar berhasil diunggah, lanjutkan dengan penyimpanan data pemesanan ke database
+            $sql = "INSERT INTO pemesanan (nama, alamat, telp, cluster, pembayaran, tgl, gambar) VALUES ('$nama', '$alamat', '$telp', '$cluster', '$pembayaran', '$tgl', '$destination')";
+            
+            if ($conn->query($sql) === TRUE) {
+                // Pemesanan berhasil disimpan ke database
+                $response = array(
+                    'status' => 'success',
+                    'message' => 'Order placed successfully.'
+                );
+            } else {
+                // Gagal menyimpan pemesanan ke database
+                $response = array(
+                    'status' => 'error',
+                    'message' => 'Failed to place order.'
+                );
+            }
+        } else {
+            // Gagal mengunggah gambar
+            $response = array(
+                'status' => 'error',
+                'message' => 'Failed to upload image.'
+            );
+        }
+    } else {
+        // Data tidak lengkap dalam permintaan
+        $response = array(
+            'status' => 'error',
+            'message' => 'Incomplete request data.'
+        );
+    }
+} else {
+    // Metode HTTP yang tidak valid
+    $response = array(
+        'status' => 'error',
+        'message' => 'Invalid request method.'
+    );
+}
+
+// Mengirimkan respons JSON ke aplikasi Android
+header('Content-Type: application/json');
+echo json_encode($response);
+
+// Menutup koneksi ke database
+$conn->close();
+?>
