37883e0348584c96b9538e6a3c64a141ce9f619c
Daffaizath12 <117889603+Daffaizath12@users.noreply.github.com>
2023-06-08 20:33:53 +0700

    POST dan GET simpan_cluster

---
+++ api/cluster_tersimpan.php
@@ -0,0 +1,44 @@
+<?php
+
+// Koneksi ke database
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'bernady';
+
+$koneksi = mysqli_connect($host, $username, $password, $database);
+if (mysqli_connect_errno()) {
+    echo "Gagal terhubung ke MySQL: " . mysqli_connect_error();
+    exit();
+}
+
+if (isset($_GET['id_user'])) {
+    $id_user = mysqli_real_escape_string($koneksi, $_GET['id_user']);
+
+    // Mengambil data dari tabel simpan_cluster berdasarkan id_user
+    $query = "SELECT simpan_cluster.*,nama_cluster, foto_cluster, harga FROM simpan_cluster LEFT JOIN cluster ON cluster.id_cluster = simpan_cluster.id_cluster WHERE id_user = '$id_user'";
+    $result = mysqli_query($koneksi, $query);
+
+    // Menyimpan data simpan_cluster yang didapatkan ke dalam array
+    $simpanClusters = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $simpanClusters[] = $row;
+    }
+
+    // Mengembalikan data simpan_cluster dalam format JSON
+    header('Content-Type: application/json');
+    echo json_encode($simpanClusters);
+} else {
+    // Jika id_user tidak ada dalam data yang diterima
+    $response = array(
+        'status' => 'error',
+        'message' => 'Parameter id_user tidak valid'
+    );
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+// Menutup koneksi database
+mysqli_close($koneksi);
+?>
--- api/tersimpan_api.php
+++ api/tersimpan_api.php
@@ -1,100 +1,52 @@
 <?php
-// Koneksi ke database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "bernady";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Koneksi ke database gagal: " . $conn->connect_error);
+// Koneksi ke database
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'bernady';
+
+$koneksi = mysqli_connect($host, $username, $password, $database);
+if (mysqli_connect_errno()) {
+    echo "Gagal terhubung ke MySQL: " . mysqli_connect_error();
+    exit();
 }
 
-// Menangani permintaan POST dari aplikasi Android untuk menambahkan produk favorit
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Memeriksa apakah semua data yang diperlukan ada dalam permintaan
-    if (isset($_POST['user_id']) && isset($_POST['id_cluster'])) {
-        $userId = $_POST['user_id'];
-        $productId = $_POST['id_cluster'];
-        
-        // Memeriksa apakah produk sudah ada di daftar favorit pengguna
-        $checkQuery = "SELECT * FROM pemesanan_rumah WHERE user_id = '$userId' AND id_cluster = '$productId'";
-        $checkResult = $conn->query($checkQuery);
-        
-        if ($checkResult->num_rows > 0) {
-            // Produk sudah ada di daftar favorit pengguna
-            $response = array(
-                'status' => 'error',
-                'message' => 'Product already exists in favorites list.'
-            );
-        } else {
-            // Menambahkan produk ke daftar favorit pengguna
-            $insertQuery = "INSERT INTO favorite_products (user_id, product_id) VALUES ('$userId', '$productId')";
-            
-            if ($conn->query($insertQuery) === TRUE) {
-                // Produk berhasil ditambahkan ke daftar favorit pengguna
-                $response = array(
-                    'status' => 'success',
-                    'message' => 'Product added to favorites successfully.'
-                );
-            } else {
-                // Gagal menambahkan produk ke daftar favorit pengguna
-                $response = array(
-                    'status' => 'error',
-                    'message' => 'Failed to add product to favorites.'
-                );
-            }
-        }
+// Mendapatkan data yang dikirim dari aplikasi
+$data = json_decode(file_get_contents('php://input'), true);
+$id_user = $data['id_user'];
+$id_cluster = $data['id_cluster'];
+
+// Cek apakah cluster sudah disimpan oleh pengguna
+$query = "SELECT * FROM simpan_cluster WHERE id_user = $id_user AND id_cluster = $id_cluster";
+$result = mysqli_query($koneksi, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    // Cluster sudah disimpan oleh pengguna
+    $response = array(
+        'status' => 'error',
+        'message' => 'Cluster sudah disimpan'
+    );
+} else {
+    // Simpan cluster ke database
+    $query = "INSERT INTO simpan_cluster (id_user, id_cluster) VALUES ($id_user, $id_cluster)";
+    if (mysqli_query($koneksi, $query)) {
+        $response = array(
+            'status' => 'success',
+            'message' => 'Cluster berhasil disimpan'
+        );
     } else {
-        // Data tidak lengkap dalam permintaan
         $response = array(
             'status' => 'error',
-            'message' => 'Incomplete request data.'
+            'message' => 'Gagal menyimpan cluster'
         );
     }
 }
 
-// Menangani permintaan GET dari aplikasi Android untuk mendapatkan daftar produk favorit
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    // Memeriksa apakah user_id ada dalam permintaan
-    if (isset($_GET['user_id'])) {
-        $userId = $_GET['user_id'];
-        
-        // Mengambil daftar produk favorit pengguna
-        $selectQuery = "SELECT * FROM simpan_cluster WHERE user_id = '$userId'";
-        $result = $conn->query($selectQuery);
-        
-        if ($result->num_rows > 0) {
-            // Menghasilkan daftar produk favorit
-            $favorites = array();
-            while ($row = $result->fetch_assoc()) {
-                $favorites[] = $row['id_cluster'];
-            }
-            
-            $response = array(
-                'status' => 'success',
-                'favorites' => $favorites
-            );
-        } else {
-            // Tidak ada produk favorit yang ditemukan
-            $response = array(
-                'status' => 'success',
-                'favorites' => array()
-            );
-        }
-    } else {
-        // Data tidak lengkap dalam permintaan
-        $response = array(
-            'status' => 'error',
-            'message' => 'Incomplete request data.'
-            );
-        }
-    }
+// Mengembalikan response dalam format JSON
+header('Content-Type: application/json');
+echo json_encode($response);
 
-    // Mengirimkan respons JSON ke aplikasi Android
-    header('Content-Type: application/json');
-    echo json_encode($response);
-
-    //menutup koneksi ke database
-    $conn->close();
+// Menutup koneksi database
+mysqli_close($koneksi);
 ?>
