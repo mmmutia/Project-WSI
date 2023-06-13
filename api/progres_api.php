c9eaa6032e08733c859ea7b33c61b904a83db0fa
Daffaizath12 <117889603+Daffaizath12@users.noreply.github.com>
2023-06-13 20:31:24 +0700

    Update progres_api.php

--- api/progres_api.php
+++ api/progres_api.php
@@ -21,21 +21,21 @@ if (mysqli_connect_errno()) {
 if (isset($_GET['id_user'])) {
     $id_user = mysqli_real_escape_string($koneksi, $_GET['id_user']);
 
-    // Mengambil data dari tabel progress berdasarkan id_user
-    $query = "SELECT * FROM proggres WHERE id_user = '$id_user'";
+    // Mengambil data dari tabel proggres berdasarkan id_user
+    $query = "SELECT proggres.*, pemesanan_rumah.id_pemesanan_rumah, pemesanan_rumah.nama_pemesan FROM proggres LEFT JOIN pemesanan_rumah ON pemesanan_rumah.id_pemesanan_rumah = proggres.id_pemesanan WHERE pemesanan_rumah.id_user = '$id_user'";
     $result = mysqli_query($koneksi, $query);
 
-    // Menyimpan data progress yang didapatkan ke dalam array
-    $progressData = array();
+    // Menyimpan data proggres yang didapatkan ke dalam array
+    $proggresData = array();
     while ($row = mysqli_fetch_assoc($result)) {
-        $progressData[] = $row;
+        $proggresData[] = $row;
     }
 
-    // Mengembalikan data progress dalam format JSON
+    // Mengembalikan data proggres dalam format JSON
     $response = array(
         'status' => 'success',
         'message' => 'Data berhasil ditemukan',
-        'data' => $progressData
+        'data' => $proggresData
     );
 
     header('Content-Type: application/json');
