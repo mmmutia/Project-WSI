dfbe7c92bc96307f44708d9648669a06671f9906
Daffaizath12 <117889603+Daffaizath12@users.noreply.github.com>
2023-06-19 10:36:16 +0700

    Update API

--- api/cluster_tersimpan.php
+++ api/cluster_tersimpan.php
@@ -29,47 +29,13 @@ if ($result->num_rows > 0) {
             'harga' => $row['harga'],
             'namacluster' => $row['nama_cluster']
         );
-
-        // Menambahkan $item ke dalam $response
-        $response[] = $item;
     }
 
     // Mengubah response menjadi format JSON
-    echo json_encode($response);
+    echo json_encode($item);
 } else {
     echo "Data tidak ditemukan.";
 }
-
-
-// if (isset($_GET['id_user'])) {
-//     $id_user = mysqli_real_escape_string($koneksi, $_GET['id_user']);
-
-//     // Mengambil data dari tabel simpan_cluster berdasarkan id_user
-//     $query = "SELECT simpan_cluster.*, nama_cluster, foto_cluster, harga FROM simpan_cluster LEFT JOIN cluster ON cluster.id_cluster = simpan_cluster.id_cluster WHERE id_user = ?";
-//     $statement = mysqli_prepare($koneksi, $query);
-//     mysqli_stmt_bind_param($statement, 's', $id_user);
-//     mysqli_stmt_execute($statement);
-//     $result = mysqli_stmt_get_result($statement);
-
-//     // Menyimpan data simpan_cluster yang didapatkan ke dalam array
-//     $simpanClusters = array();
-//     while ($row = mysqli_fetch_assoc($result)) {
-//         $simpanClusters[] = $row;
-//     }
-
-//     // Mengembalikan data simpan_cluster dalam format JSON
-//     header('Content-Type: application/json');
-//     echo json_encode($simpanClusters);
-// } else {
-//     // Jika id_user tidak ada dalam data yang diterima
-//     $response = array(
-//         'status' => 'error',
-//         'message' => 'Parameter id_user tidak valid'
-//     );
-
-//     header('Content-Type: application/json');
-//     echo json_encode($response);
-// }
 
 // Menutup koneksi database
 mysqli_close($koneksi);
