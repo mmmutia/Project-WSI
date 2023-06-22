61eac70e81424c25d756863074ad3b6b22f7e0f0
ferinaintan <ferinaintan435@gmail.com>
2023-06-22 11:58:06 +0700

    update progres

--- api/pembayaran-dp_api.php
+++ api/pembayaran-dp_api.php
@@ -13,20 +13,27 @@ if ($conn->connect_error) {
 // Mendapatkan data dari request POST
 $idPemesananRumah = $_POST['id_pemesanan_rumah'];
 $tglPembayaranDp = $_POST['tgl_pembayaran_dp'];
+$buktidp = $_FILES['bukti_pembayaran_dp'];
 
-// Meng-handle file gambar yang di-upload
-$buktiPembayaranDp = $_FILES['bukti_pembayaran_dp'];
-
-$targetDir = "/img/pembayaran_dp/"; // Ganti dengan direktori upload yang sesuai di server Anda
-$targetFile = $targetDir . basename($buktiPembayaranDp["name"]);
+// Mengunggah gambar ke direktori tujuan
+$uploadDir = '../img/pembayaran_dp/';
+// Memeriksa apakah direktori tujuan sudah ada
+if (!is_dir($uploadDir)) {
+    // Jika belum ada, buat direktori
+    mkdir($uploadDir, 0755, true);
+}
+$uploadedFile = $_FILES['bukti_pembayaran_dp']['tmp_name'];
+$fileName = explode(".", $_FILES['bukti_pembayaran_dp']['name'])[0];
+$fileName = $fileName."-".date('ymdHis').".jpg";
+$destination = $uploadDir . $fileName;
 
 // Memindahkan file gambar ke direktori tujuan
-if (move_uploaded_file($buktiPembayaranDp["tmp_name"], $targetFile)) {
+if (move_uploaded_file($uploadedFile, $destination)) {
     // File berhasil di-upload
 
     // Menyimpan data ke dalam tabel pembayaran_dp
     $sql = "INSERT INTO pembayaran_dp (id_pembayaran_dp, id_pemesanan_rumah, tgl_pembayaran_dp, bukti_pembayaran_dp)
-            VALUES (null, '$idPemesananRumah', '$tglPembayaranDp', '$targetFile')";
+            VALUES (null, '$idPemesananRumah', '$tglPembayaranDp', '$fileName')";
 
     if ($conn->query($sql) === TRUE) {
         // Data berhasil disimpan
--- api/pembayaran-inhouse_api.php
+++ api/pembayaran-inhouse_api.php
@@ -13,20 +13,27 @@ if ($conn->connect_error) {
 // Mendapatkan data dari request POST
 $idPemesananRumah = $_POST['id_pemesanan_rumah'];
 $tglPembayaranInhouse = $_POST['tgl_pembayaran_inhouse'];
+$buktiinhouse = $_FILES['bukti_pembayaran_inhouse'];
 
-// Meng-handle file gambar yang di-upload
-$buktiPembayaranInhouse = $_FILES['bukti_pembayaran_inhouse'];
-
-$targetDir = "/img/bukti_inhouse/"; // Ganti dengan direktori upload yang sesuai di server Anda
-$targetFile = $targetDir . basename($buktiPembayaranInhouse["name"]);
+// Mengunggah gambar ke direktori tujuan
+$uploadDir = '../img/bukti_inhouse/';
+// Memeriksa apakah direktori tujuan sudah ada
+if (!is_dir($uploadDir)) {
+    // Jika belum ada, buat direktori
+    mkdir($uploadDir, 0755, true);
+}
+$uploadedFile = $_FILES['bukti_pembayaran_inhouse']['tmp_name'];
+$fileName = explode(".", $_FILES['bukti_pembayaran_inhouse']['name'])[0];
+$fileName = $fileName."-".date('ymdHis').".jpg";
+$destination = $uploadDir . $fileName;
 
 // Memindahkan file gambar ke direktori tujuan
-if (move_uploaded_file($buktiPembayaranInhouse["tmp_name"], $targetFile)) {
+if (move_uploaded_file($uploadedFile, $destination)) {
     // File berhasil di-upload
 
     // Menyimpan data ke dalam tabel pembayaran_dp
     $sql = "INSERT INTO pembayaran_inhouse (id_pembayaran_inhouse, id_pemesanan_rumah, tgl_pembayaran_inhouse, bukti_pembayaran_inhouse)
-            VALUES (null, '$idPemesananRumah', '$tglPembayaranInhouse', '$targetFile')";
+            VALUES (null, '$idPemesananRumah', '$tglPembayaranInhouse', '$fileName')";
 
     if ($conn->query($sql) === TRUE) {
         // Data berhasil disimpan
